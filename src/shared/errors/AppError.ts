1a2529cd0b8326cdd3c013cca870491d7276d896
Junior Paula <slucivaldo509@gmail.com>
2022-02-03 19:19:53 -0300

    Create class AppError to customize error

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,12 @@
+/** class for application error handling */
+class AppError {
+  readonly message: string;
+  readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 /** express intance */
 const app = express();
@@ -10,6 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+/** middleware to intercept all errors */
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error.',
+    });
+  },
+);
 
 /** start on server */
 const port = process.env.PORT || 5005;
