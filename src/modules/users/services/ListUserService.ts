d3d255cd97f0eb30fa16a993be88f6131fe920bf
Junior Paula <slucivaldo509@gmail.com>
2022-02-04 22:23:09 -0300

    Create Service to list all users

---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,13 @@
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+class ListUserService {
+  async execute(): Promise<User[]> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const users = await usersRepository.find();
+    return users;
+  }
+}
+
+export default ListUserService;
